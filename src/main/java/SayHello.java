ff8ec0fe3f6d1f293aa1457234e5cf88b02a1672
aleksandr_khamitski <al.khamitski@gmail.com>
2021-10-30 19:30:03 +0300

    [AK] SayHello.java, new line has been added;

--- src/main/java/SayHello.java
+++ src/main/java/SayHello.java
@@ -1,5 +1,6 @@
 public class SayHello {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
+        System.out.println("First push via terminal!");
     }
 }
